b29af3280eced99a15fe29fd581150bc66a5e45f
Jeje34 <pastorjeremy34@gmail.com>
2015-12-17 00:01:57 +0100

    Ajout logo + tracage ligne 1

--- app/src/main/java/com/bezierstransports/LineMapActivity.java
+++ app/src/main/java/com/bezierstransports/LineMapActivity.java
@@ -1,6 +1,7 @@
 package com.bezierstransports;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,6 +17,8 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.util.ArrayList;
 
@@ -86,6 +89,7 @@ public class LineMapActivity extends FragmentActivity {
                     .position(new LatLng(station.getLatitude(), station.getLongitude()))
                     .title(station.getStationName())));
 
+            // zoom and move camera to see all the markers
             mMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
 
                 public void onMapLoaded() {
@@ -100,5 +104,48 @@ public class LineMapActivity extends FragmentActivity {
                 }
             });
         }
+
+        drawLine();
+    }
+
+    private void drawLine() {
+        PolylineOptions rectOptions = new PolylineOptions()
+                .add(new LatLng(43.34576, 3.217365))
+                .add(new LatLng(43.345954, 3.216877))
+                .add(new LatLng(43.348932, 3.220735))
+                .add(new LatLng(43.350356, 3.218777))
+                .add(new LatLng(43.350904, 3.219306))
+                .add(new LatLng(43.351731, 3.220312))
+                .add(new LatLng(43.353076, 3.221369))
+                .add(new LatLng(43.355371, 3.222028))
+                .add(new LatLng(43.356609, 3.221757))
+                .add(new LatLng(43.357030, 3.222071))
+                .add(new LatLng(43.357773, 3.224026))
+                .add(new LatLng(43.358372, 3.224686))
+                .add(new LatLng(43.358975, 3.224874))
+                .add(new LatLng(43.359282, 3.224812))
+                .add(new LatLng(43.360007, 3.225461))
+                .add(new LatLng(43.359614, 3.226691))
+                .add(new LatLng(43.358185, 3.225621))
+                .add(new LatLng(43.355104, 3.226459))
+                .add(new LatLng(43.355143, 3.226598))
+                .add(new LatLng(43.355880, 3.227161))
+                .add(new LatLng(43.357128, 3.227805))
+                .add(new LatLng(43.361153, 3.230895))
+                .add(new LatLng(43.360295, 3.233663))
+                .add(new LatLng(43.359577, 3.234275 ))
+                .add(new LatLng(43.359273, 3.234699))
+                .add(new LatLng(43.360178, 3.235992))
+                .add(new LatLng(43.361274, 3.235461))
+                .add(new LatLng(43.361929, 3.234324))
+                .add(new LatLng(43.361952, 3.232994))
+                .add(new LatLng(43.362395, 3.231827))
+                .width(5)
+                .color(Color.BLUE)
+                .geodesic(true);
+
+        Polyline polyline = mMap.addPolyline(rectOptions);
+
+
     }
 }
